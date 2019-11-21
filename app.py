50a79fc3f237fd79098a7a0860cd3e39c198d650
Hamza Musse <35120165+musse32@users.noreply.github.com>
2019-11-21 00:07:04 -0500

    Second commit

---
+++ app.py
@@ -0,0 +1,21 @@
+## adoption_site.py
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'mykey' #Secret key for the forms
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
