46d17ca3d72684041b81f4b25ea78fc58ea7b2a4
hamzamusse <35120165+musse32@users.noreply.github.com>
2020-07-23 14:57:15 -0400

    route pdf

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 ## adoption_site.py
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 
 app = Flask(__name__)
 
@@ -20,6 +20,11 @@ def projects():
 @app.route('/covid_map')
 def map():
     return render_template('folium_covid_map.html')
+
+@app.route('/hamza-musse-resume-engineering.pdf/')
+def show_static_pdf():
+    with open('/hamza-musse-resume-engineering.pdf', 'rb') as static_file:
+        return send_file(static_file, attachment_filename='file.pdf')
 
 if __name__ == '__main__':
     app.run(debug=True)
