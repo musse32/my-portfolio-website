2c38c70fc6eec193d414aca7b2fa2929b27969f9
Hamza Musse <35120165+musse32@users.noreply.github.com>
2020-08-23 15:40:01 -0400

    add files

---
+++ README.md
@@ -0,0 +1,2 @@
+# Portfolio of personal data science and web development projects. 
+## Deployed at https://hamza-musse-portfolio.herokuapp.com/
---
+++ app.py
@@ -0,0 +1,30 @@
+## adoption_site.py
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'mykey' #Secret key for the forms
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/covid_map')
+def map():
+    return render_template('folium_covid_map.html')
+
+@app.route('/resume')
+def resume():
+    return render_template('resume.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
